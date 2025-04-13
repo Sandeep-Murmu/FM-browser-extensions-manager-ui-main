7a8e6ab0ac5d1dedaeffda275ad510737c90706f
Sandeep Murmu <sandeepmurmu99@gmail.com>
2025-04-13 13:13:04 +0530

    add text-button component

--- src/components/extention-header.js
+++ src/components/extention-header.js
@@ -1,16 +1,19 @@
 import logo from "../assets/images/logo.svg";
 import logoTheme from "../assets/images/icon-sun.svg";
-import ButtonIcon from "./cstmButton";
+import ButtonIcon from "./icon-Button";
+import ButtonText from "./text-button";
 export default function ExtentionHeader() {
   return (
     <>
       <div className="extention-header p-4">
         <img src={logo} alt="logo" />
         <ButtonIcon
-          btnClass="extention-header__btn"
+          btnClass="button--icon"
           src={logoTheme}
           alt="logo"
         />
+
+        <ButtonText btnContent="Active" btnClass="button--text" />
       </div>
     </>
   );
