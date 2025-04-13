376826fb87810dc1d21cbf6e9cdf79f53caa8019
Sandeep Murmu <sandeepmurmu99@gmail.com>
2025-04-13 12:49:49 +0530

    add icon-button component

--- src/components/extention-header.js
+++ src/components/extention-header.js
@@ -1,14 +1,16 @@
 import logo from "../assets/images/logo.svg";
 import logoTheme from "../assets/images/icon-sun.svg";
-
+import ButtonIcon from "./cstmButton";
 export default function ExtentionHeader() {
   return (
     <>
       <div className="extention-header p-4">
         <img src={logo} alt="logo" />
-        <button className="extention-header__btn">
-          <img src={logoTheme} alt="logo" />
-        </button>
+        <ButtonIcon
+          btnClass="extention-header__btn"
+          src={logoTheme}
+          alt="logo"
+        />
       </div>
     </>
   );
