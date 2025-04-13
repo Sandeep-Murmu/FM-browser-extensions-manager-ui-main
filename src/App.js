d6253575da3a6e89a614eec2d3f2291d6e5a1ce4
Sandeep Murmu <sandeepmurmu99@gmail.com>
2025-04-13 13:39:22 +0530

    import extention data from data.json

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import ExtentionHeader from "./components/extention-header";
 import ExtententionTab from "./components/extention-tab";
+import extentionsData from "./data.json";
 
 function App() {
+  extentionsData.forEach((extention) => {
+    console.log(extention.name);
+  });
+
   return (
     <div className="App">
       <div className="container py-5">
